54d910075c92b65f34f4cb22de695acd22ea101b
shant0786 <rm.shanto786@gmail.com>
2024-12-03 22:03:09 +0600

    models:all models schemas are added

---
+++ src/models/CategoryModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const DataSchema = mongoose.Schema(
+  {
+    categoryName: { type: String, unique: true },
+    categoryImg: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const CategoryModel = mongoose.model("categories", DataSchema);
+
+module.exports = CategoryModel;
