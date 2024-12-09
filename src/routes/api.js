03c9b780dd5ee1b9f1d2e6914f990aba12266758
shant0786 <rm.shanto786@gmail.com>
2024-12-09 15:54:06 +0600

    utility:Email-Utility, Token-Utility, AuthMiddleware added

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,7 +3,6 @@ const PrductController = require("../controllers/ProductController");
 const router = express.Router();
 
 //  Product routes
-
 router.get("/ProductBrandList", PrductController.ProductBrandList);
 router.get("/ProductCategoryList", PrductController.ProductCategoryList);
 router.get("/ProductSliderList", PrductController.ProductSliderList);
@@ -27,5 +26,11 @@ router.get(
 router.get("/ProductDetails/:ProductID", PrductController.ProductDetails);
 router.get("/ProductReviewList/:ProductID", PrductController.ProductReviewList);
 router.get("/ProductReviewList/:ProductID", PrductController.ProductReviewList);
+
+
+
+// User Routes
+
+
 
 module.exports = router;
---
+++ src/services/UserServices.js
@@ -0,0 +1,12 @@
+const UserLoginService = async () => {
+}
+const VerifyLoginService = async () => {
+}
+const LogoutService = async () => {
+}
+const CreateProfileService = async () => {
+}
+const UpdateProfileService = async () => {
+}
+const ReadProfileService = async () => {
+}
